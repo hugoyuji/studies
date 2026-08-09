cb2160f43a63b151c79446d407c116d4184f0ee6
Hugo <domotohugo@gmail.com>
2026-08-09 00:58:54 -0300

    feat: adiciona service de aluno

---
+++ java/gym-api/src/main/java/br/com/gym_api/service/AlunoService.java
@@ -0,0 +1,47 @@
+package br.com.gym_api.service;
+
+import br.com.gym_api.model.Aluno;
+import br.com.gym_api.repository.AlunoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    AlunoRepository repository;
+
+    public AlunoService(AlunoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Aluno cadastrar(Aluno aluno){
+        return repository.save(aluno);
+    }
+
+    public Aluno buscarPorId(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Aluno não encontrado."));
+    }
+
+    public List<Aluno> listar(){
+        return repository.findAll();
+    }
+
+    public Aluno alterar(Long id, Aluno aluno){
+        Aluno alunoExistente = buscarPorId(id);
+
+        alunoExistente.setNome(aluno.getNome());
+        alunoExistente.setEmail(aluno.getEmail());
+        alunoExistente.setTelefone(aluno.getTelefone());
+        alunoExistente.setDataNascimento(aluno.getDataNascimento());
+        alunoExistente.setDataMatricula(aluno.getDataMatricula());
+        alunoExistente.setAtivo(aluno.isAtivo());
+
+        return repository.save(alunoExistente);
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
