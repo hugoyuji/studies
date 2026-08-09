cc8787ea02e54382a02ba97120ed8505a2a06fac
Hugo <domotohugo@gmail.com>
2026-08-09 01:18:19 -0300

    refactor: ajusta service de aluno

--- java/gym-api/src/main/java/br/com/gym_api/service/AlunoService.java
+++ java/gym-api/src/main/java/br/com/gym_api/service/AlunoService.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Service
 public class AlunoService {
 
-    AlunoRepository repository;
+    private final AlunoRepository repository;
 
     public AlunoService(AlunoRepository repository) {
         this.repository = repository;
