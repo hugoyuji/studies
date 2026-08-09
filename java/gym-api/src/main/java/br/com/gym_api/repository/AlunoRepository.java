5cd31119aa08cebc800569a4de7b3989c6ce98a2
Hugo <domotohugo@gmail.com>
2026-08-09 01:00:29 -0300

    chore: adiciona anotação repository

--- java/gym-api/src/main/java/br/com/gym_api/repository/AlunoRepository.java
+++ java/gym-api/src/main/java/br/com/gym_api/repository/AlunoRepository.java
@@ -2,6 +2,8 @@ package br.com.gym_api.repository;
 
 import br.com.gym_api.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 }
