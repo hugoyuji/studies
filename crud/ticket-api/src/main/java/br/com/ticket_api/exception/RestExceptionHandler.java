b3bc9c2d39891c4e51031005aff3e79ed95bca93
Hugo <domotohugo@gmail.com>
2026-08-30 19:42:35 -0300

    test: adiciona testes manuais das rotas da api e trata exceção de desserialização

--- crud/ticket-api/src/main/java/br/com/ticket_api/exception/RestExceptionHandler.java
+++ crud/ticket-api/src/main/java/br/com/ticket_api/exception/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.ticket_api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -40,6 +41,17 @@ public class RestExceptionHandler {
         body.put("status", HttpStatus.BAD_REQUEST.value());
         body.put("error", "Bad Request");
         body.put("errors", erros);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException exception){
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Bad Request");
+        body.put("message", "O corpo da requisição contém dados inválidos ou mal formatados.");
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
